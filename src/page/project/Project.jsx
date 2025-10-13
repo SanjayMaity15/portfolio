eda8f2fc3d93a3b2f8dbe6cce4cfe03a1bef81bd
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2025-10-13 17:27:28 +0530

    Project secction started

--- src/page/project/Project.jsx
+++ src/page/project/Project.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 const Project = () => {
   return (
-    <div>Project</div>
-  )
+		<section>
+			<div className="section-container text-white">
+				<h3 className="text-center mt-12 text-4xl font-bold">
+					Projects
+				</h3>
+				<p className='text-center text-gray-300 mt-4 mb-8'>
+					A selection of projects that
+					demonstrate my skills in building real-world web
+					applications with clean code and modern technologies.
+				</p>
+			</div>
+		</section>
+  );
 }
 
 export default Project
